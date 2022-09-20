51f62581d4545faf7b36c36907a35b779d3bb528
Andrew^-^ <99921755+Maestro2315@users.noreply.github.com>
2022-09-20 17:00:02 +0400

    Initial commit

---
+++ src/main/java/com/example/BurgerStorage/BurgerStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.BurgerStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerStorageApplication.class, args);
+	}
+
+}
